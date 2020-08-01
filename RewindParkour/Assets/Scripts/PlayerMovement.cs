459f0b59803213b0f71902b8073aac2da484e94e
Shan Memon <shankmemon@gmail.com>
2020-08-01 15:17:51 -0500

    Init

---
+++ RewindParkour/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = default;
+
+    private Vector2 movementInput = default;
+    private Rigidbody rb;
+
+    private void Start() {
+        movementInput = new Vector2();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        GetInput();
+    }
+
+    private void FixedUpdate() {
+        DoMovement();
+    }
+
+    private void DoMovement()
+    {
+        rb.velocity = transform.forward * movementInput * movementSpeed * Time.deltaTime;
+    }
+
+    private void GetInput()
+    {
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+    }
+}
