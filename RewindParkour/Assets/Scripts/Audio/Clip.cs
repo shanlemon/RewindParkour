b126dffa4f50bbe218dc0bf2db4aa53b103d3a48
Shan Memon <shankmemon@gmail.com>
2020-08-02 17:42:07 -0500

    Added audio manager and imported dotween

---
+++ RewindParkour/Assets/Scripts/Audio/Clip.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Clip 
+{
+	public AudioClip AudioClip;
+	[Range(0f, 2f)]
+	public float volume = 1;
+	[Range(.1f, 3f)]
+	public float pitch = 1;
+
+}
---
+++ RewindParkour/Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Sound", menuName = "Sound")]
+public class Sound : ScriptableObject
+{
+	public Clip[] clips;
+	public bool loop;
+
+	public float fadeInTime = 0f;
+	public float fadeOutTime = 0f;
+
+	[HideInInspector]
+	public AudioSource source;
+
+	// Return a random clip from the list
+	public Clip GetRandomClip() {
+		if (clips.Length == 0) {
+			//Debug.LogError("Cannot GetRandomClip for Sound " + this + ": no clips found.");
+			return null;
+		}
+
+		int randIndex = Random.Range(0, clips.Length);
+		return clips[randIndex];
+	}
+
+	public Clip GetClip(int index) {
+		if (index < 0 || index > clips.Length -1)
+		{
+			Debug.LogError("The index " + index + " for clip " + name + " does not exist.");
+			return null;
+		}
+		return clips[index];
+	}
+
+}
