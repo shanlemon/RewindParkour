703ccc4857878b6a09ee3e09c1efecddfe5d2b2a
edwin-silerio <edwin_silerio@utexas.edu>
2020-08-03 22:04:58 -0500

    Testing multiple portals

--- RewindParkour/Assets/Scripts/Portal/PortalTextureSetup.cs
+++ RewindParkour/Assets/Scripts/Portal/PortalTextureSetup.cs
@@ -8,30 +8,19 @@ using UnityEngine;
 /// </summary>
 public class PortalTextureSetup : MonoBehaviour
 {
-    [SerializeField] private Material cameraMatA = default;
-    [SerializeField] private Camera cameraA = default;
-    
-    [SerializeField] private Material cameraMatB = default;
-    [SerializeField] private Camera cameraB = default;
+    [SerializeField] private Material cameraMat = default;
+    [SerializeField] private Camera camera = default;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (cameraA.targetTexture != null)
+        if (camera.targetTexture != null)
         {
-            cameraA.targetTexture.Release();
+            camera.targetTexture.Release();
         }
 
-        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        cameraMatA.mainTexture = cameraA.targetTexture;
-
-        if (cameraB.targetTexture != null)
-        {
-            cameraB.targetTexture.Release();
-        }
-
-        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        cameraMatB.mainTexture = cameraB.targetTexture;
+        camera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMat.mainTexture = camera.targetTexture;
         
     }
 
