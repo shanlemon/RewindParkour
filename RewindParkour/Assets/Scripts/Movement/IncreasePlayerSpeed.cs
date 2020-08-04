76619ffa9aac50b0bba020d3a4b96fd7ddf77a9c
hakhan0301 <hakhan0301@gmail.com>
2020-08-04 17:40:39 -0500

    increase speed script

---
+++ RewindParkour/Assets/Scripts/Movement/IncreasePlayerSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreasePlayerSpeed : MonoBehaviour {
+	public IncreasePlayerSpeed Instance { get; private set; }
+
+	[SerializeField] private PlayerLocomotion playerLocomotion;
+	[SerializeField] private float defaultAccelerationMultiplier = 1 + 2f / 10f;
+	[SerializeField] private float defaultMaxSpeedMultiplier = 1 + 2f / 15f;
+
+	public void Awake() {
+		Instance = this;
+	}
+
+	public void IncreaseSpeed() {
+		playerLocomotion.Acceleration *= defaultAccelerationMultiplier;
+		playerLocomotion.MaxSpeed *= defaultMaxSpeedMultiplier;
+	}
+}
