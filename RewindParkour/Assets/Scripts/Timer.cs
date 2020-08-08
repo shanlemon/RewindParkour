f52b85d19882f0686cd3f5bfd35f18ac97c7a033
edwin-silerio <edwin_silerio@utexas.edu>
2020-08-08 02:35:27 -0500

    Added time storage and display

--- RewindParkour/Assets/Scripts/Timer.cs
+++ RewindParkour/Assets/Scripts/Timer.cs
@@ -7,15 +7,39 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timeDisplay = default;
+    [SerializeField] private int numLevels = 3;
 
     private bool isTiming = false;
+    private string allTimesKey = "AllTimes";
 
     // Used to set the time in hours, minutes and sec format
     private const int hours = 216000;
     private const int minutes = 3600;
     private const int seconds = 60;
 
+
     public float PlayerTime { get; set; }
+    public float[] AllTimes { get; set;}
+
+    private void Start()
+    {
+        AllTimes = PlayerPrefsX.GetFloatArray(allTimesKey, 0f, numLevels);
+
+        // AllTimes doesn't have the right number of levels so make a new array
+        if (AllTimes.Length != numLevels)
+        {
+            float[] temp = new float[numLevels];
+            for (int i = 0; i < AllTimes.Length; i++)
+            {
+                temp[i] = AllTimes[i];
+            }
+        }
+
+        foreach(float time in AllTimes)
+        {
+            print($"Time: {time}");
+        }
+    }
 
     public void StartTimer()
     {
@@ -43,12 +67,18 @@ public class Timer : MonoBehaviour
         }
     }
 
-    public void StopTimer()
+    public void StopTimer(int section, TriggerPad triggerPad)
     {
         // Only stop timing if we're already timing
         if (isTiming)
         {
             isTiming = false;
+            if (PlayerTime < AllTimes[section])
+            {
+                AllTimes[section] = PlayerTime;
+                PlayerPrefsX.SetFloatArray(allTimesKey, AllTimes);
+            }
+            triggerPad.DisplayTime(PlayerTime);
             PlayerTime = 0f;
             timeDisplay.text = "00:00:00";
         }
